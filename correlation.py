9f514aad795492136be72f64e5a171656cc75077
Éverton Assis <eassis.vieira@gmail.com>
2021-01-14 22:14:18 -0300

    *-*

--- correlation.py
+++ correlation.py
@@ -1,16 +1,19 @@
-import os
 import re
 import json
 import statistics
 import functools
 
 from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
 
 BLOCK_RATES_FILENAME = 'block_rates.json'
 WIENER_INDEXES_FILENAME = 'wienerIndexes.json'
+GRAPH_DEGREES_FILENAME = 'graphDegres.json'
+PRINT_FOLDER = 'prints'
 
 block_rate = json.load(open(BLOCK_RATES_FILENAME, 'r'))
 wiener_indexes = json.load(open(WIENER_INDEXES_FILENAME, 'r'))
+graph_degrees = json.load(open(GRAPH_DEGREES_FILENAME, 'r'))
 
 block_rate_list = []
 wiener_indexes_list = []
@@ -25,32 +28,45 @@ network_names = functools.reduce(
     lambda prev, value: repl(value, prev), block_rate.keys(), set())
 
 
-def get_values_of(name, v_dict):
+def get_values_of(n, v_dict):
     return list(map(lambda v: v_dict[v], filter(
-        lambda value: value.startswith(name), sorted(v_dict.keys()))))
+        lambda value: value.startswith(n), sorted(v_dict.keys()))))
 
 
 corr_list = []
 
+
+def create_image(name, corr, xs, ys, dgs):
+    plt.figure(figsize=(15, 10))
+    plt.plot(xs, ys, 'ro')
+    plt.xlabel('Índice de Wiener')
+    plt.ylabel('Taxa de bloqueio')
+    plt.grid(True)
+    plt.title(f'{name}  -  {corr:.4f}')
+    for idx, dg in enumerate(dgs):
+        plt.annotate(f'{dg:.1f}', (xs[idx], ys[idx]))
+    plt.savefig(fname=f'{PRINT_FOLDER}/{name}.png')
+    plt.close()
+
+
+print('Creating images')
 for name in network_names:
     xs = get_values_of(name, wiener_indexes)
     ys = get_values_of(name, block_rate)
+    dgs = get_values_of(name, graph_degrees)
     corr, _ = pearsonr(xs, ys)
+    create_image(name, corr, xs, ys, dgs)
     corr_list.append((name, abs(corr)))
-    print(f'Correlação de {name}: {corr}')
-
 
 for key in sorted(block_rate.keys()):
-
     block_rate_list.append(block_rate[key])
     wiener_indexes_list.append(wiener_indexes[key])
 
-print(f'Correlação geral: {pearsonr(wiener_indexes_list, block_rate_list)}')
+
+for name, corr in corr_list:
+    print(f'{name:17} => {corr:.08f}')
+
 
 print(f'Média: {statistics.fmean(map(lambda x: x[1],corr_list))}')
 
-corr_list.sort(key=lambda x: x[1])
-bigger = corr_list[-2:]
-smaller = corr_list[:2]
-print(bigger)
-print(smaller)
+print(f'Correlação geral: {pearsonr(wiener_indexes_list, block_rate_list)[0]}')
--- wiener.py
+++ wiener.py
@@ -10,24 +10,30 @@ json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.ends
     '.json') and not pos_json.endswith('nodes.json')]
 
 with open('./wienerIndexes.json', 'w') as wienerIndexes:
+    with open('./graphDegres.json', 'w') as graphDegres:
 
-    wienerIndexesDict = {}
+        wienerIndexesDict = {}
+        graphDegreeDict = {}
 
-    for fileName in json_files:
-        print(fileName)
-        with open('./Topologias/' + fileName, 'r') as json_file:
+        for fileName in json_files:
+            print(fileName)
+            with open('./Topologias/' + fileName, 'r') as json_file:
 
-            JSONLinks = json.load(json_file)
+                JSONLinks = json.load(json_file)
 
-            G = nx.Graph()
+                G = nx.Graph()
 
-            G.add_weighted_edges_from(
-                (elem['From'], elem['To'], 1)
-                for elem in JSONLinks
-            )
+                G.add_weighted_edges_from(
+                    (elem['From'], elem['To'], 1)
+                    for elem in JSONLinks
+                )
 
-            wienerIndex = nx.wiener_index(G)
+                wienerIndex = nx.wiener_index(G)
+                graphDegree = sum(
+                    map(lambda v: v[1], G.degree())) / len(G.nodes)
 
-            wienerIndexesDict[fileName] = wienerIndex
+                wienerIndexesDict[fileName] = wienerIndex
+                graphDegreeDict[fileName] = graphDegree
 
-    json.dump(wienerIndexesDict, wienerIndexes, sort_keys=True)
+        json.dump(wienerIndexesDict, wienerIndexes, sort_keys=True)
+        json.dump(graphDegreeDict, graphDegres, sort_keys=True)
