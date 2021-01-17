56e8aaa9d683569a7f3a30555dc77135889689e4
Éverton Assis <eassis.vieira@gmail.com>
2021-01-17 13:18:26 -0300

    :D

--- correlation.py
+++ correlation.py
@@ -1,4 +1,5 @@
 import re
+import csv
 import json
 import statistics
 import functools
@@ -11,29 +12,15 @@ WIENER_INDEXES_FILENAME = 'wienerIndexes.json'
 GRAPH_DEGREES_FILENAME = 'graphDegres.json'
 PRINT_FOLDER = 'prints'
 
-block_rate = json.load(open(BLOCK_RATES_FILENAME, 'r'))
-wiener_indexes = json.load(open(WIENER_INDEXES_FILENAME, 'r'))
-graph_degrees = json.load(open(GRAPH_DEGREES_FILENAME, 'r'))
-
-block_rate_list = []
-wiener_indexes_list = []
-
 
 def repl(value, prev):
     prev.add(re.sub(r'_links_\d*\.json', '', value))
     return prev
 
 
-network_names = functools.reduce(
-    lambda prev, value: repl(value, prev), block_rate.keys(), set())
-
-
 def get_values_of(n, v_dict):
     return list(map(lambda v: v_dict[v], filter(
         lambda value: value.startswith(n), sorted(v_dict.keys()))))
-
-
-corr_list = []
 
 
 def create_image(name, corr, xs, ys, dgs):
@@ -49,24 +36,30 @@ def create_image(name, corr, xs, ys, dgs):
     plt.close()
 
 
-print('Creating images')
-for name in network_names:
-    xs = get_values_of(name, wiener_indexes)
-    ys = get_values_of(name, block_rate)
-    dgs = get_values_of(name, graph_degrees)
-    corr, _ = pearsonr(xs, ys)
-    create_image(name, corr, xs, ys, dgs)
-    corr_list.append((name, abs(corr)))
+def main():
+    block_rate = json.load(open(BLOCK_RATES_FILENAME, 'r'))
+    wiener_indexes = json.load(open(WIENER_INDEXES_FILENAME, 'r'))
+    graph_degrees = json.load(open(GRAPH_DEGREES_FILENAME, 'r'))
 
-for key in sorted(block_rate.keys()):
-    block_rate_list.append(block_rate[key])
-    wiener_indexes_list.append(wiener_indexes[key])
+    network_names = functools.reduce(
+        lambda prev, value: repl(value, prev), block_rate.keys(), set())
+
+    corr_list = []
+    with open('correlations.csv', 'w') as fcsv:
+        writer = csv.DictWriter(fcsv, ['Nome da rede', 'Correlação'])
+        for name in network_names:
+            xs = get_values_of(name, wiener_indexes)
+            ys = get_values_of(name, block_rate)
+            dgs = get_values_of(name, graph_degrees)
+            corr, _ = pearsonr(xs, ys)
+            create_image(name, corr, xs, ys, dgs)
+            corr_list.append((name, abs(corr)))
+            writer.writerow({'Nome da rede': name, 'Correlação': corr})
+            print(f'{name:16} => {corr:.08f}')
+
+    print(
+        f'Média das correlações: {statistics.fmean(map(lambda x: x[1],corr_list)):.08f}')
 
 
-for name, corr in corr_list:
-    print(f'{name:17} => {corr:.08f}')
-
-
-print(f'Média: {statistics.fmean(map(lambda x: x[1],corr_list))}')
-
-print(f'Correlação geral: {pearsonr(wiener_indexes_list, block_rate_list)[0]}')
+if __name__ == '__main__':
+    main()
